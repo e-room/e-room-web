73661ce5c7cabca36e0a757308f8aa11b10dd834
hyojin <injulme@naver.com>
2022-08-11 00:40:49 +0900

    next storybook 연동 설정 추가

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,20 @@
 module.exports = {
-  "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../stories/**/*.stories.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
   ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
-  }
-}
+  framework: "@storybook/react",
+  core: {
+    builder: "@storybook/builder-webpack5",
+  },
+  // webpackFinal: async (config) => {
+  //   config.module.rules.unshift({
+  //     test: /\.svg$/,
+  //     use: ["@svgr/webpack"],
+  //   });
+
+  //   return config;
+  // },
+};
