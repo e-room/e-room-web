3175db4d22ea9e8f292695455ee93ad1bd4c7108
hyojin <injulme@naver.com>
2022-08-27 22:52:25 +0900

    TextArea 컴포넌트 구현

---
+++ components/Input/TextArea.js
@@ -0,0 +1,47 @@
+import PropTypes from "prop-types";
+import styled from "@emotion/styled";
+
+const StyledInputText = styled.textarea`
+  display: flex;
+  align-items: center;
+  padding: 16px;
+
+  border: 1px solid var(--gray-4);
+  border-radius: 12px;
+
+  resize: none;
+
+  ::placeholder {
+    color: var(--gray-3);
+  }
+  &:focus {
+    outline: 1px solid var(--primary-1);
+  }
+`;
+
+export default function TextArea({
+  value,
+  onChange,
+  placeholder = "필드를 입력해주세요.",
+  width,
+  height,
+  ...props
+}) {
+  return (
+    <StyledInputText
+      placeholder={placeholder}
+      onChange={onChange}
+      value={value}
+      style={{ width, height }}
+      {...props}
+    />
+  );
+}
+
+TextArea.propTypes = {
+  value: PropTypes.string,
+  onChange: PropTypes.func,
+  placeholder: PropTypes.string,
+  width: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  height: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+};
