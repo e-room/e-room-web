8d4636b2d255fb7c07fa94fc38b4b23fd4732608
hyojin <injulme@naver.com>
2022-09-12 17:40:18 +0900

    review form state 값 정의

--- states/index.js
+++ states/index.js
@@ -14,3 +14,28 @@ export const reviewStepState = atom({
   key: "reviewStepState",
   default: 0,
 });
+
+export const reviewFormState = atom({
+  key: "reviewFormState",
+  default: {
+    address: null, // 주소
+    lineNumber: null, // 상세주소 (동)
+    roomNumber: null, // 상세주소 (호)
+    residenceType: null, //
+    residencePeriod: null,
+    floorHeight: null,
+    deposit: null,
+    monthlyRent: null,
+    managementFee: null,
+    netLeasableArea: null,
+    managementFeeDescription: null,
+    trafficScore: null,
+    buildingComplexScore: null,
+    surroundingScore: null,
+    internalScore: null,
+    livingLocationScore: null,
+    advantageKeywordList: null,
+    disadvantageKeywordList: null,
+    residenceSatisfaction: null,
+  },
+});
