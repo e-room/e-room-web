d183bce4e080bd8c707ecb74faa061dc5a4197f8
HB <hbnhb@kakao.com>
2022-09-20 22:32:54 +0900

    전희범의 두 번째 커밋 테스트

--- stories/bottomPopup.stories.js
+++ stories/bottomPopup.stories.js
@@ -12,5 +12,5 @@ const Template = (args) => {
 export const Default = Template.bind({});
 Default.args = {
   title: "제목을 입력해주세요", /* 안녕하세요! */
-  subTitle: "보조 텍스트를 입력해주세요",
+  subTitle: "보조 텍스트를 입력해주세요", /* 안녕하세요! */
 };
