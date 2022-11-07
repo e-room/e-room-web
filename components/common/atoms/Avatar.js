0430b66f691fbb070b1306bafb5e0dd8519fcd13
injulme <injulme0309@naver.com>
2022-11-07 23:32:56 +0900

    avatar 컴포넌트 스타일 수정

--- components/common/atoms/Avatar.js
+++ components/common/atoms/Avatar.js
@@ -1,26 +1,13 @@
 import PropTypes from "prop-types";
 import styled from "@emotion/styled";
 
-const StyledAvatar = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-
-  img {
-    border: 1px solid var(--gray-3);
-    border-radius: 100%;
-    width: ${(props) => props.size}px;
-    height: ${(props) => props.size}px;
-  }
-`;
-
 const SIZES = {
   md: 24,
   lg: 64,
 };
-export default function Avatar({ size = "md", img, children }) {
+export default function Avatar({ size = "md", img, children, ...props }) {
   return (
-    <StyledAvatar size={SIZES[size]}>
+    <StyledAvatar size={SIZES[size]} {...props}>
       {img ? <img src={img} width={SIZES[size]} height={SIZES[size]} /> : children}
     </StyledAvatar>
   );
@@ -30,3 +17,13 @@ Avatar.propTypes = {
   size: PropTypes.oneOf(["md", "lg"]),
   // img: PropTypes.element,
 };
+
+const StyledAvatar = styled.div`
+  display: flex;
+  img {
+    border: 1px solid var(--gray-3);
+    border-radius: 100%;
+    width: ${(props) => props.size}px;
+    height: ${(props) => props.size}px;
+  }
+`;
