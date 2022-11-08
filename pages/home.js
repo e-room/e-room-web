aacccfd95e4e56bbdbafad81e53fd711d29c1221
injulme <injulme0309@naver.com>
2022-11-08 22:08:26 +0900

    마커 클러스터러 기능 구현

--- pages/home.js
+++ pages/home.js
@@ -14,79 +14,94 @@ import CheckBox from "components/common/atoms/CheckBox";
 import { pageTitleState } from "states";
 import { useSetRecoilState } from "recoil";
 import MarkerPng from "assets/marker.png";
-
 const initial = {
-  lat: 37.5173319258532,
-  lng: 127.047377408384,
+  lat: 33.453705,
+  lng: 126.570677,
 };
-
-const getLocation = [
-  {
-    coordinate: {
-      longitude: 124.450701,
-      latitude: 32.570667,
-    },
-    buildingId: 1,
-  },
-  {
-    coordinate: {
-      longitude: 132.483,
-      latitude: 40.57,
-    },
-    buildingId: 2,
-  },
-  {
-    coordinate: {
-      longitude: 130.4501,
-      latitude: 38.57,
-    },
-    buildingId: 3,
-  },
-];
-
 const MainMap = () => {
   const setPageTitleState = useSetRecoilState(pageTitleState);
   const kakaoMapRef = useRef(null); // 지도 container ref
   const map = useRef(null);
   // const [mapCenter, setMapCenter] = useState({ lat: initial.lat, lng: initial.lng });
-
   //map불러오기
   const initMap = useCallback(() => {
     let imageSrc = MarkerPng.src;
     let imageSize = new kakao.maps.Size(61, 68);
     let imageOption = { offset: new kakao.maps.Point(30, 48) };
-
     var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
-
     if (kakaoMapRef.current && !map.current) {
       const initialMap = new kakao.maps.Map(kakaoMapRef.current, {
         center: new kakao.maps.LatLng(initial.lat, initial.lng),
         level: 6,
       });
-
+      // 마커 클러스터러를 생성합니다
+      let clusterer = new kakao.maps.MarkerClusterer({
+        map: initialMap, // 마커들을 클러스터로 관리하고 표시할 지도 객체
+        averageCenter: true, // 클러스터에 포함된 마커들의 평균 위치를 클러스터 마커 위치로 설정
+        minLevel: 4, // 클러스터 할 최소 지도 레벨
+        styles: [
+          {
+            width: "61px",
+            height: "68px",
+            backgroundImage: "url(num-pin-one.svg)",
+            backgroundRepeat: "no-repeat",
+            borderRadius: "8px",
+            color: "var(--primary-1)",
+            textAlign: "center",
+            fontWeight: "700",
+            lineHeight: "20px",
+            fontSize: "14px",
+          },
+          {
+            width: "61px",
+            height: "68px",
+            backgroundImage: "url(num-pin-two.svg)",
+            backgroundRepeat: "no-repeat",
+            borderRadius: "8px",
+            color: "var(--primary-1)",
+            textAlign: "center",
+            fontWeight: "700",
+            lineHeight: "20px",
+            fontSize: "14px",
+          },
+          {
+            width: "61px",
+            height: "68px",
+            backgroundImage: "url(num-pin-three.svg)",
+            backgroundRepeat: "no-repeat",
+            borderRadius: "8px",
+            color: "var(--primary-1)",
+            textAlign: "center",
+            fontWeight: "700",
+            lineHeight: "20px",
+            fontSize: "14px",
+          },
+        ],
+      });
       // TODO: 동기로 변경하고 setTimeout 제거
+      const markers = [];
       setTimeout(() => {
-        new kakao.maps.Marker({
-          position: new kakao.maps.LatLng(initial.lat, initial.lng),
-          map: initialMap,
-          image: markerImage,
+        getLocation.forEach((value) => {
+          let marker = new kakao.maps.Marker({
+            position: new kakao.maps.LatLng(value.lat, value.lng),
+            image: markerImage,
+          });
+          marker.setMap(initialMap);
+          markers.push(marker);
         });
-      }, [500]);
-
+        clusterer.addMarkers(markers);
+      }, [1000]);
       map.current = initialMap;
     }
   }, [kakaoMapRef.current]);
-
   useEffect(() => {
     setPageTitleState(null);
   }, []);
-
   useEffect(() => {
     if (window.kakao) {
       initMap();
     }
   }, [initMap]);
-
   //나의 위치로 가게 해주는 함수
   const setMyPosition = () => {
     if (navigator.geolocation) {
@@ -94,7 +109,6 @@ const MainMap = () => {
         const lat = position.coords.latitude; // 위도
         const lng = position.coords.longitude; // 경도
         // setMapCenter({ lat, lng });
-
         let myPosition = new kakao.maps.LatLng(lat, lng);
         map.current.setCenter(myPosition);
       });
@@ -108,16 +122,14 @@ const MainMap = () => {
   const zoomOut = () => {
     map.current.setLevel(map.current.getLevel() + 1);
   };
-
   const [popupVisible, setPopupVisible] = useState(false);
   const onHideClick = () => {
     setPopupVisible(false);
   };
-
   return (
     <Fragment>
       <Script
-        src={`https://dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_APP_KEY}&autoload=false`}
+        src={`https://dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_APP_KEY}&autoload=false&libraries=clusterer`}
         onLoad={() => kakao.maps.load(initMap)}
       />
       {popupVisible && (
@@ -186,18 +198,15 @@ const MainMap = () => {
     </Fragment>
   );
 };
-
 const MapWrapper = styled.div`
   width: 100%;
   height: 100vh;
 `;
 const MapContainer = styled.div``;
-
 const Container = styled.div`
   height: calc(100vh - 112px);
   overflow: hidden !important;
 `;
-
 const FilterItem = styled.div`
   position: absolute;
   right: 20px;
@@ -231,15 +240,88 @@ const Contents = styled.div`
   justify-content: space-between;
   padding: 0 6px;
 `;
-
 const SubText = styled.div`
   ${Body2Bold}
   color: var(--black);
 `;
-
 const ButtonGroup = styled.div`
   display: flex;
   gap: 8px;
 `;
-
 export default MainMap;
+const dummy = () => {
+  return Array(100)
+    .fill()
+    .map(() => {
+      return {
+        lat: 33.450705,
+        lng: 126.570677,
+      };
+    });
+};
+const getLocation = [
+  {
+    lat: 33.450705,
+    lng: 126.570677,
+  },
+  {
+    lat: 33.450936,
+    lng: 126.569477,
+  },
+  {
+    lat: 33.450879,
+    lng: 126.56994,
+  },
+  {
+    lat: 33.451393,
+    lng: 126.570738,
+  },
+  {
+    lat: 33.452493,
+    lng: 126.572938,
+  },
+  {
+    lat: 33.451373,
+    lng: 126.570538,
+  },
+  {
+    lat: 33.45129,
+    lng: 126.571701,
+  },
+  {
+    lat: 33.451457,
+    lng: 126.57101,
+  },
+  {
+    lat: 33.45131,
+    lng: 126.570018,
+  },
+  {
+    lat: 33.451099,
+    lng: 126.570799,
+  },
+  {
+    lat: 33.451393,
+    lng: 126.570738,
+  },
+  {
+    lat: 33.451393,
+    lng: 126.570738,
+  },
+  {
+    lat: 33.451393,
+    lng: 126.570738,
+  },
+  {
+    lat: 33.451393,
+    lng: 126.570738,
+  },
+  {
+    lat: 33.451393,
+    lng: 126.570738,
+  },
+  {
+    lat: 33.451393,
+    lng: 126.570738,
+  },
+];
