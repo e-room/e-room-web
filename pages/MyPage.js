d1eb8353db3ccc54d6620b23828fb18524c25b6c
injulme <injulme0309@naver.com>
2022-11-25 21:30:28 +0900

    mypage: myinfo 표출

--- pages/MyPage.js
+++ pages/MyPage.js
@@ -2,6 +2,10 @@ import { useEffect } from "react";
 import { useSetRecoilState } from "recoil";
 import AppLayout from "components/common/AppLayout";
 import { pageTitleState } from "states";
+import styled from "@emotion/styled";
+import Avatar from "components/common/atoms/Avatar";
+import Img from "assets/avatar/64.png";
+import { Body1Bold, Caption2 } from "styles/typography";
 
 export default function mypage() {
   const setPageTitleState = useSetRecoilState(pageTitleState);
@@ -9,5 +13,34 @@ export default function mypage() {
   useEffect(() => {
     setPageTitleState("내정보");
   }, []);
-  return <AppLayout>내 정보</AppLayout>;
+  return (
+    <AppLayout>
+      <MyInfo>
+        <Avatar size={"lg"} img={Img.src} />
+        <div style={{ width: "100%" }}>
+          <Nickname>하품하는 망아지</Nickname>
+          <Email>hbnhb@kakao.com</Email>
+        </div>
+      </MyInfo>
+    </AppLayout>
+  );
 }
+
+const MyInfo = styled.div`
+  height: 104px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  gap: 16px;
+  padding: 20px;
+`;
+
+const Nickname = styled.div`
+  ${Body1Bold}
+  color: var(--black);
+`;
+const Email = styled.div`
+  ${Caption2}
+  color: var(--black);
+`;
