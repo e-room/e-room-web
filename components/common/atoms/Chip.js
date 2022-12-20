8f8bac77dd110d72c5656d166e471e3b8bd84d06
정효진 <injulme@cntech.pro>
2022-12-20 18:41:05 +0900

    chip component type 추가

--- components/common/atoms/Chip.js
+++ components/common/atoms/Chip.js
@@ -1,9 +1,11 @@
 import PropTypes from "prop-types";
 import styled from "@emotion/styled";
+import { css } from "@emotion/react";
 
-export default function Chip({ label, children }) {
+export default function Chip({ label, type = "primary", children }) {
+  const colorStyle = COLORS[type];
   return (
-    <StyledChip>
+    <StyledChip colorStyle={colorStyle}>
       <div>{label ?? children}</div>
     </StyledChip>
   );
@@ -11,9 +13,12 @@ export default function Chip({ label, children }) {
 
 Chip.propTypes = {
   label: PropTypes.string,
+  type: PropTypes.oneOf(["primary", "secondary", "tertiary"]),
 };
 
 const StyledChip = styled.div`
+  ${(p) => p.colorStyle}
+
   display: flex;
   flex-direction: row;
   align-items: center;
@@ -22,13 +27,27 @@ const StyledChip = styled.div`
 
   height: 20px;
 
-  background: var(--primary-1);
+  background: var(--chip-bg-color);
   border-radius: 16px;
 
   div {
     font-weight: 600;
-    color: var(--white);
+    color: var(--chip-label-color);
     font-size: 12px;
     line-height: 16px;
   }
 `;
+const COLORS = {
+  primary: css`
+    --chip-bg-color: var(--primary-1);
+    --chip-label-color: var(--white);
+  `,
+  secondary: css`
+    --chip-bg-color: var(--secondary-1);
+    --chip-label-color: var(--black);
+  `,
+  tertiary: css`
+    --chip-bg-color: var(--gray-4);
+    --chip-label-color: var(--black);
+  `,
+};
