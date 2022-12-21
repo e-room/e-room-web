89ac1f3acce8c47c2606bbf7d345b8170ad74b69
injulme <injulme0309@naver.com>
2022-12-22 00:16:42 +0900

    scroll top 기능 구현중

--- pages/building/[id].js
+++ pages/building/[id].js
@@ -8,6 +8,7 @@ import BuildingMap from "components/building/BuildingMap";
 import { buildingSelector, imageViewState } from "states/buidlingAtom";
 import { useRecoilState, useRecoilValue } from "recoil";
 import Slider from "components/building/Slider";
+import { useCallback, useEffect, useLayoutEffect, useRef, useState } from "react";
 
 export default () => {
   const router = useRouter();
@@ -19,15 +20,33 @@ export default () => {
   };
   // router.query.id
 
+  //////////////////////////////////////////
+  const [scrollActive, setScrollActive] = useState(false);
+  const layoutRef = useRef(null);
+  const handleScroll = useCallback(({ target }) => {
+    console.log(target.scrollTop);
+    if (target.scrollTop > 500) setScrollActive(true);
+    else setScrollActive(false);
+  }, []);
+
+  useLayoutEffect(() => {
+    if (layoutRef.current) {
+      layoutRef.current.addEventListener("scroll", handleScroll);
+      return () => layoutRef.current.removeEventListener("scroll", handleScroll);
+    }
+  }, []);
+  //////////////////////////////////////////
+
   return (
     <AppLayout>
-      <Container>
+      <Container ref={layoutRef}>
         {showImgDetail && <Slider data={dummyImages} onClose={onCloseImg} />}
         <BuildingMap />
         <BuildingInfo />
         <ImageView />
         <ReviewList />
       </Container>
+      <TestBox isTop={scrollActive}>{scrollActive ? "픽스됨~~~" : "아직 ㅠㅠ"}</TestBox>
     </AppLayout>
   );
 };
@@ -35,4 +54,17 @@ export default () => {
 const Container = styled.div`
   height: calc(100vh - 112px);
   overflow: scroll;
+`;
+
+const TestBox = styled.div`
+  position: fixed;
+  top: ${(p) => (p.isTop ? "0" : "500px")};
+  right: 20px;
+  z-index: 199999;
+  padding: 10px;
+  background: lightseagreen;
+  border-radius: 6px;
+  color: #fff;
+  border: 1px solid rgb(23, 122, 117);
+  text-align: left;
 `;
