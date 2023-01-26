ea3ba6fe3aad00ca09a2e8612939155ecf3e81e7
injulme <injulme0309@naver.com>
2023-01-26 11:51:25 +0900

    운영환경에선 쿠키 못가져옴...

--- pages/review/write.js
+++ pages/review/write.js
@@ -6,12 +6,12 @@ import { useEffect } from "react";
 import Router from "next/router";
 
 export default function reviewWrite() {
-  useEffect(() => {
-    if (!getCookie("accessToken")) {
-      // TODO: 계정 인증 후 없으면 리다이렉트
-      Router.push(`/login`);
-    }
-  }, [getCookie("accessToken")]);
+  // useEffect(() => {
+  //   if (!getCookie("accessToken")) {
+  //     // TODO: 계정 인증 후 없으면 리다이렉트
+  //     Router.push(`/login`);
+  //   }
+  // }, [getCookie("accessToken")]);
 
   // TODO: reviewform1, 2,,, 로직 개선
   return (
