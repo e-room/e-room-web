468ba07905d99c87e54b955f128298456ac49391
injulme <injulme0309@naver.com>
2023-01-26 11:48:23 +0900

    임시조치) 리뷰쓸 때 쿠키가 없으면 로그인 리다이렌트

--- pages/review/write.js
+++ pages/review/write.js
@@ -1,18 +1,19 @@
 import ReviewLayout from "components/review/ReviewLayout";
 import ReviewForm1 from "components/review/ReviewForm1";
+import { getCookie } from "cookies-next";
 
-import Login from "../login";
+import { useEffect } from "react";
+import Router from "next/router";
 
 export default function reviewWrite() {
-  // useEffect(() => {
-  //   if (!isLogin) {
-  //     // TODO: 계정 인증 후 없으면 리다이렉트
-  //     Router.push(`/login`);
-  //   }
-  // }, [isLogin]);
+  useEffect(() => {
+    if (!getCookie("accessToken")) {
+      // TODO: 계정 인증 후 없으면 리다이렉트
+      Router.push(`/login`);
+    }
+  }, [getCookie("accessToken")]);
 
   // TODO: reviewform1, 2,,, 로직 개선
-
   return (
     <ReviewLayout>
       <ReviewForm1 />
