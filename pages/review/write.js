30bf12f83c56dd0b9f95bb0d4dd1e3e0167a4bf3
injulme <injulme0309@naver.com>
2023-02-09 20:47:06 +0900

    리뷰쓰기 로그인 검증 로직 추가

--- pages/review/write.js
+++ pages/review/write.js
@@ -4,8 +4,16 @@ import { getCookie } from "cookies-next";
 
 import { useEffect } from "react";
 import Router from "next/router";
+import axios from "axios";
 
 export default function reviewWrite() {
+  useEffect(() => {
+    const accessResponse = axios.get(`/apis/token/valid`);
+    if (!accessResponse.data || !accessResponse.data.isValid) {
+      console.log("로그인을 해주세요.");
+    }
+  }, []);
+
   // useEffect(() => {
   //   if (!getCookie("accessToken")) {
   //     // TODO: 계정 인증 후 없으면 리다이렉트
