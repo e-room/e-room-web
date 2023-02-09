4789a4821960f58cf0b6168f744a512a87883089
injulme <injulme0309@naver.com>
2023-02-09 20:46:13 +0900

    찜하기 목록 api 연동 (계정 작업 후 테스트)

--- pages/favorites.js
+++ pages/favorites.js
@@ -2,11 +2,33 @@ import styled from "@emotion/styled";
 
 import AppLayout from "components/common/AppLayout";
 import BuildingList from "components/building/BuildingList";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export default function favorites() {
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState({});
+  const getData = async () => {
+    setLoading(true);
+    await axios
+      .get(`/apis/member/favorite`, {
+        headers: { mocking: 239 },
+      })
+      .then((res) => setData(res.data));
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  if (loading) return <div>loading...</div>;
+
   return (
     <AppLayout pageTitle={"찜한 자취방"}>
-      <Container>{/* <BuildingList data={data} /> */}</Container>
+      <Container>
+        {data?.content?.length > 0 && <BuildingList data={data} />}
+      </Container>
     </AppLayout>
   );
 }
