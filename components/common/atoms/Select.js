e9a21b87f861a7f88ca0c1d101bd69d3d44e7cb8
injulme <injulme0309@naver.com>
2023-02-16 22:35:37 +0900

    select 컴포넌트 수정

--- components/common/atoms/Select.js
+++ components/common/atoms/Select.js
@@ -1,172 +1,131 @@
-import PropTypes from "prop-types";
-import styled from "@emotion/styled";
-import ArrowIcon from "../../../assets/arrow.svg";
-import { useState } from "react";
-import { Body2, Body2Bold, Body3 } from "../../../styles/typography";
+import ArrowDownIcon from "../../../assets/arrow-down.svg";
+import ArrowUpIcon from "../../../assets/arrow-up.svg";
+import { Body2 } from "../../../styles/typography";
+import Select, { components } from "react-select";
 
-// TODO: 검색기능 및 여러가지 기능 추가 필요(조사 후 진행)
-// TODO: hover할때 위 아래 네모모양으로 표시됨
-export default function Select({
-  placeholder = "필드를 선택해주세요.",
-  items = [],
+// size = md || lg
+export default function CustomSelect({
+  size = "lg",
+  options,
   value,
   onChange,
-  label,
-  unit,
-  ...props
 }) {
-  const [isVisible, setIsVisible] = useState(false);
-  const onOptionsShow = () => {
-    setIsVisible(!isVisible);
+  // TODO: padding size 15 -> 16
+  const colorStyles = {
+    container: (base) => {
+      return { ...base, width: "100%" };
+    },
+    control: (base, state) => {
+      return {
+        ...base,
+        padding: size === "lg" ? 15 : "9px 15px",
+        borderRadius: 12,
+
+        outline: "none",
+        borderWidth: 1,
+        borderColor:
+          state.isFocused && state.menuIsOpen
+            ? `var(--primary-1)`
+            : `var(--gray-4)`,
+        boxShadow: "none",
+        "&:hover": `var(--primary-1)`,
+      };
+    },
+    placeholder: (base) => {
+      return {
+        ...base,
+        Body2,
+        color: `var(--gray-3)`,
+        margin: 0,
+      };
+    },
+    singleValue: (base) => {
+      return { ...base, fontWeight: 500, fontSize: "16px", lineHeight: "24px" };
+    },
+    input: (base) => {
+      return {
+        ...base,
+        margin: 0,
+        padding: 0,
+      };
+    },
+    valueContainer: (base) => {
+      return { ...base, padding: 0 };
+    },
+    menu: (base) => {
+      return {
+        ...base,
+        boxShadow: "0px 0px 12px rgba(0, 0, 0, 0.08)",
+        borderRadius: 12,
+        backgroundColor: `var(--white)`,
+      };
+    },
+    menuList: (base) => {
+      return { ...base, padding: 0 };
+    },
+    option: (base, state) => {
+      const isActive = (state.isFocused && !state.data) || state.isSelected;
+      return {
+        ...base,
+        Body2,
+        fontWeight: isActive ? `700` : `500`,
+        color: isActive ? `var(--primary-1)` : `var(--black)`,
+        padding: 16,
+        backgroundColor: isActive ? `var(--primary-6)` : `var(--white)`,
+        ":nth-of-type(1)": {
+          borderRadius: "12px 12px 0px 0px",
+        },
+        ":nth-last-of-type(1)": {
+          borderRadius: "0px 0px 12px 12px",
+        },
+        "&:hover": {
+          backgroundColor: `var(--primary-6)`,
+          ":nth-of-type(1)": {
+            borderRadius: "12px 12px 0px 0px",
+          },
+          ":nth-last-of-type(1)": {
+            borderRadius: "0px 0px 12px 12px",
+          },
+        },
+        ":active": {
+          backgroundColor: `var(--primary-6)`,
+          ":nth-of-type(1)": {
+            borderRadius: "12px 12px 0px 0px",
+          },
+          ":nth-last-of-type(1)": {
+            borderRadius: "0px 0px 12px 12px",
+          },
+        },
+      };
+    },
   };
 
-  // TODO: blur 시 옵션 리스트 숨김으로 변경
-  // onOptionChange 할 때 blur(focus out)이 먼저 일어나서 값을 변경할 수 없음
-  // ref나 따로 연결해서 사용해야 함
-  const onBlur = (e) => {
-    console.log("on blur");
-    setTimeout(() => setIsVisible(false), 100);
+  const DropdownIndicator = (props) => {
+    const { selectProps } = props;
+    return (
+      <components.DropdownIndicator {...props}>
+        {props.isFocused && selectProps.menuIsOpen ? (
+          <ArrowUpIcon width={11} height={6} fill={`var(--primary-1)`} />
+        ) : (
+          <ArrowDownIcon width={11} height={6} fill={`var(--gray-1)`} />
+        )}
+      </components.DropdownIndicator>
+    );
   };
-
-  // TODO: 공통컴포넌트로 분리 필요
 
   return (
-    <StyledSelect>
-      <div style={{ display: "flex", flexDirection: "column" }}>
-        {label && <TextLabel>{label}</TextLabel>}
-        <div style={{ display: "flex", alignItems: "center" }}>
-          <SelectField onClick={onOptionsShow} focus={isVisible}>
-            <OptionField defaultOption={value}>
-              {value ? `${value}년` : placeholder}
-            </OptionField>
-            <div className="toggle-icon">
-              <ArrowIcon width={11} height={6} fill={`var(--gray-1)`} />
-            </div>
-            <OptionList visible={isVisible}>
-              {items.map((option) => {
-                return (
-                  <Option
-                    key={option.value}
-                    onClick={() => {
-                      onChange(option);
-                      setIsVisible(false);
-                    }}
-                    isSelect={value && option.value === value}
-                  >
-                    {option.label}
-                  </Option>
-                );
-              })}
-            </OptionList>
-          </SelectField>
-
-          {unit && <TextUnit>{unit}</TextUnit>}
-        </div>
-      </div>
-    </StyledSelect>
+    <Select
+      styles={colorStyles}
+      components={{
+        DropdownIndicator,
+        IndicatorSeparator: null,
+      }}
+      value={value}
+      onChange={onChange}
+      options={options}
+      placeholder={"필드를 선택해주세요."}
+      isSearchable={false}
+      selected={value}
+    />
   );
 }
-
-const OptionField = styled.div`
-  overflow: hidden;
-  text-overflow: ellipsis;
-  white-space: nowrap;
-
-  color: ${(p) => (p.defaultOption ? `var(--black)` : `var(--gray-3)`)};
-`;
-
-const SelectField = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 16px;
-  width: 100%;
-
-  border: 1px solid var(--gray-4);
-  border-radius: 12px;
-  background: transparent;
-
-  position: relative;
-
-  ${Body2}
-
-  .toggle-icon {
-    display: flex;
-  }
-
-  svg {
-    margin-left: 10px;
-  }
-
-  ${(p) =>
-    p.focus &&
-    `border: 1px solid var(--primary-1);
-    svg {
-      fill: var(--primary-1);
-    }`}
-`;
-
-const OptionList = styled.div`
-  display: ${(p) => (p.visible ? "block" : "none")};
-  box-shadow: 0px 0px 12px rgba(0, 0, 0, 0.08);
-  background: var(--white);
-  border-radius: 12px;
-  width: 100%;
-  position: absolute;
-  z-index: 9;
-  top: 59px;
-  left: 0;
-`;
-
-const Option = styled.button`
-  border: none;
-  background: transparent;
-  width: 100%;
-  text-align: left;
-  padding: 16px;
-
-  overflow: hidden;
-
-  ${(p) => (p.isSelect ? Body2Bold : Body2)}
-
-  &:hover {
-    background: var(--gray-4);
-  }
-
-  ${(p) =>
-    p.isSelect &&
-    `
-    color: var(--primary-1);
-    background: var(--primary-6);
-    :nth-of-type(1) {
-      border-radius: 12px 12px 0px 0px;
-    }
-    :nth-last-of-type(1) {
-      border-radius: 0px 0px 12px 12px;
-    }
-  `}
-`;
-
-const StyledSelect = styled.div`
-  &:focus {
-    outline: none;
-    border: 1px solid var(--primary-1);
-    svg {
-      fill: var(--primary-1);
-    }
-  }
-`;
-
-const TextLabel = styled.div`
-  ${Body2Bold}
-
-  margin-bottom: 4px;
-`;
-
-const TextUnit = styled.div`
-  ${Body3}
-  white-space: nowrap;
-  margin-left: 8px;
-  width: 30px;
-  min-width: 30px;
-`;
