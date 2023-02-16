ed90a7b9f03c9461bb5aef2715523d0ca767ccbc
injulme <injulme0309@naver.com>
2023-02-16 22:36:26 +0900

    좋아요 api 연동

--- components/building/reviewItems/LikeField.js
+++ components/building/reviewItems/LikeField.js
@@ -1,11 +1,25 @@
 import styled from "@emotion/styled";
+import axios from "axios";
 import Icon from "components/common/atoms/Icon";
 import { Caption1 } from "styles/typography";
 
 export default ({ value, isLike, setIsLike }) => {
+  const reviewId = value.baseReviewDto.reviewId;
+
+  const onLike = async () => {
+    await axios
+      .put(`/apis/building/room/review/like/${reviewId}`)
+      .then((response) => {
+        console.log("onLike success", response.data);
+      })
+      .catch((error) => {
+        console.log("onLike failed", error);
+      });
+  };
+
   return (
     <LikeField favorite={isLike}>
-      <div style={{ display: "flex" }} onClick={() => setIsLike(!isLike)}>
+      <div style={{ display: "flex" }} onClick={() => onLike()}>
         <Icon icon={"thumb-stroke"} size={"sm"} />
         <div className="text">
           추천{" "}
