375a19cfecdd3d0d4635ff6e2daaaa9883f477fe
injulme <injulme0309@naver.com>
2023-02-17 20:24:23 +0900

    건물보기 지도 로직 개선

--- components/building/BuildingMap.js
+++ components/building/BuildingMap.js
@@ -1,69 +1,56 @@
-import React, {
-  useEffect,
-  useState,
-  useRef,
-  useCallback,
-  Fragment,
-} from "react";
-import Script from "next/script";
+import React, { useEffect, useState } from "react";
 import styled from "@emotion/styled";
 import MarkerPng from "assets/marker4.png";
 
 const BuildingMap = ({ building }) => {
-  const kakaoMapRef = useRef(null); // 지도 container ref
-
   const initial = {
     lat: building.coordinate.latitude,
     lng: building.coordinate.longitude,
   };
 
-  //map불러오기
-  const initMap = useCallback(() => {
-    let imageSrc = MarkerPng.src;
-    let imageSize = new kakao.maps.Size(61, 68);
-    let imageOption = { offset: new kakao.maps.Point(30, 48) };
+  const [mapLoaded, setMapLoaded] = useState(false);
 
-    var markerImage = new kakao.maps.MarkerImage(
-      imageSrc,
-      imageSize,
-      imageOption
-    );
+  useEffect(() => {
+    const $script = document.createElement("script");
+    $script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_APP_KEY}&autoload=false&libraries=clusterer`;
+    $script.addEventListener("load", () => setMapLoaded(true));
+    document.head.appendChild($script);
+  }, []);
 
-    if (kakaoMapRef.current && kakaoMapRef) {
-      const initialMap = new kakao.maps.Map(kakaoMapRef.current, {
+  useEffect(() => {
+    if (!mapLoaded) return;
+
+    kakao.maps.load(() => {
+      const imageSrc = MarkerPng.src;
+      const imageSize = new kakao.maps.Size(61, 68);
+      const imageOption = { offset: new kakao.maps.Point(30, 48) };
+
+      const markerImage = new kakao.maps.MarkerImage(
+        imageSrc,
+        imageSize,
+        imageOption
+      );
+
+      const container = document.getElementById("map");
+      const options = {
         center: new kakao.maps.LatLng(initial.lat, initial.lng),
         level: 3,
         draggable: false,
         scrollwheel: false,
         disableDoubleClick: true,
         disableDoubleClickZoom: true,
+      };
+
+      const initialMap = new kakao.maps.Map(container, options);
+      new kakao.maps.Marker({
+        position: new kakao.maps.LatLng(initial.lat, initial.lng),
+        map: initialMap,
+        image: markerImage,
       });
+    });
+  }, [mapLoaded]);
 
-      setTimeout(() => {
-        new kakao.maps.Marker({
-          position: new kakao.maps.LatLng(initial.lat, initial.lng),
-          map: initialMap,
-          image: markerImage,
-        });
-      }, [500]);
-    }
-  }, [kakaoMapRef.current]);
-
-  useEffect(() => {
-    if (window.kakao) {
-      initMap();
-    }
-  }, []);
-
-  return (
-    <Fragment>
-      <Script
-        src={`https://dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_APP_KEY}&autoload=false`}
-        onLoad={() => kakao.maps.load(initMap)}
-      />
-      <MapWrapper id="map" ref={kakaoMapRef}></MapWrapper>
-    </Fragment>
-  );
+  return <MapWrapper id="map" />;
 };
 
 const MapWrapper = styled.div`
