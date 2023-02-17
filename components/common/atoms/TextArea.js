e15d3f74bfb1f253927295b4cbeb17402f3ee1d5
injulme <injulme0309@naver.com>
2023-02-17 22:43:25 +0900

    QA9-1 text_area ui 완료

--- components/common/atoms/TextArea.js
+++ components/common/atoms/TextArea.js
@@ -1,22 +1,60 @@
 import PropTypes from "prop-types";
 import styled from "@emotion/styled";
+import { Body2, Caption1 } from "styles/typography";
+import { useEffect, useState } from "react";
 
-export default function TextArea({
-  value,
-  onChange,
-  placeholder = "필드를 입력해주세요.",
-  width,
-  height,
-  ...props
-}) {
+export default function TextArea(props) {
+  const {
+    value,
+    onChange,
+    placeholder = "필드를 입력해주세요.",
+    width,
+    height,
+    caption,
+    count,
+    countUnit,
+  } = props;
+
+  const [status, setStatus] = useState(null);
+
+  const onBlur = (e) => {
+    if (!count) return;
+    if (!value || value.length < count) {
+      setStatus("danger-1");
+    } else {
+      setStatus("success");
+    }
+  };
+
+  useEffect(() => {
+    if (!count || !status || !value) return;
+
+    if (value.length < count) {
+      setStatus("danger-1");
+    } else {
+      setStatus("success");
+    }
+  }, [value]);
+
   return (
-    <StyledInputText
-      placeholder={placeholder}
-      onChange={onChange}
-      defaultValue={value || ""}
-      style={{ width, height }}
-      {...props}
-    />
+    <>
+      <StyledInputText
+        placeholder={placeholder}
+        onChange={onChange}
+        defaultValue={value || ""}
+        style={{ width, height }}
+        onBlur={onBlur}
+        status={status}
+        {...props}
+      />
+      <CaptionField status={status}>
+        <div className="caption">{caption}</div>
+        <div className="count">
+          {count && `${value ? value.length : 0}/${count}`}
+          {countUnit}
+        </div>
+      </CaptionField>
+    </>
   );
 }
 
@@ -29,11 +67,15 @@ TextArea.propTypes = {
 };
 
 const StyledInputText = styled.textarea`
+  ${Body2}
+
+  box-sizing: border-box;
   display: flex;
   align-items: center;
   padding: 16px;
 
-  border: 1px solid var(--gray-4);
+  border: ${(p) =>
+    p.status ? `1px solid var(--${p.status})` : `1px solid var(--gray-4)`};
   border-radius: 12px;
 
   resize: none;
@@ -47,6 +89,28 @@ const StyledInputText = styled.textarea`
   }
   &:focus {
     outline: none;
-    border: 1px solid var(--primary-1);
+    border: ${(p) => !p.status && `1px solid var(--primary-1)`};
+  }
+`;
+const CaptionField = styled.div`
+  display: flex;
+  justify-content: space-between;
+  gap: 8px;
+  margin: 4px;
+
+  .caption {
+    ${Caption1}
+    color: ${(p) => (p.status ? `var(--${p.status})` : `var(--gray-2)`)};
+
+    word-break: break-word;
+    text-overflow: ellipsis;
+    overflow: hidden;
+    display: -webkit-box;
+    -webkit-line-clamp: 1;
+    -webkit-box-orient: vertical;
+  }
+  .count {
+    ${Caption1}
+    color: ${(p) => (p.status ? `var(--${p.status})` : `var(--gray-2)`)};
   }
 `;
