eb2cebc8c5946d70c794190a6c33facc9114a632
injulme <injulme0309@naver.com>
2023-02-20 23:06:20 +0900

    QA17 loading animation 적용

--- components/common/Loading.js
+++ components/common/Loading.js
@@ -1,3 +1,35 @@
+import { useEffect, useRef } from "react";
+import lottie from "lottie-web";
+import styled from "@emotion/styled";
+import loading from "assets/lottie/circle_loading-primary.json";
+
 export default () => {
-  return <div style={{ backgroundColor: "red" }}>loading</div>;
+  const likecontainer = useRef();
+  useEffect(() => {
+    lottie.loadAnimation({
+      container: likecontainer.current,
+      renderer: "svg",
+      loop: true,
+      autoplay: true,
+      animationData: loading,
+    });
+  }, []);
+  return (
+    <Container>
+      <Item ref={likecontainer} />
+    </Container>
+  );
 };
+
+const Container = styled.div`
+  width: 100vw;
+  height: 95vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Item = styled.div`
+  width: 128px;
+  height: 128px;
+`;
