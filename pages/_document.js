92b870bbd5a30767838e8fe2da17b7258d47af34
injulme <injulme0309@naver.com>
2023-02-21 16:53:35 +0900

    document 아이콘 관련 설정

--- pages/_document.js
+++ pages/_document.js
@@ -11,12 +11,60 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <meta charSet="utf-8" />
+          <link rel="manifest" href="/manifest.json" />
+          <link
+            href="images/favicons/favicon-16x16.png"
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+          />
+          <link
+            href="images/favicons/favicon-32x32.png"
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+          />
+          <link
+            rel="apple-touch-icon"
+            href="images/icons/icon-192x192.png"
+          ></link>
+          <meta name="msapplication-TileColor" content="#53ac8e"></meta>
 
-          {/* <link rel="manifest" href="/manifest.json" />
-          <link href="/64.png" rel="icon" type="image/png" sizes="16x16" />
-          <link href="/64.png" rel="icon" type="image/png" sizes="32x32" />
-          <link rel="apple-touch-icon" href="/64.png"></link>
-          <meta name="theme-color" content="#53ac8e" /> */}
+          <link
+            rel="apple-touch-startup-image"
+            href="images/splashscreens/iphone5_splash.png"
+            media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+          ></link>
+          <link
+            rel="apple-touch-startup-image"
+            href="images/splashscreens/iphone6_splash.png"
+            media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+          ></link>
+          <link
+            rel="apple-touch-startup-image"
+            href="images/splashscreens/iphoneplus_splash.png"
+            media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
+          ></link>
+          <link
+            rel="apple-touch-startup-image"
+            href="images/splashscreens/iphonex_splash.png"
+            media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
+          ></link>
+          <link
+            rel="apple-touch-startup-image"
+            href="images/splashscreens/ipad_splash.png"
+            media="(min-device-width: 768px) and (max-device-width: 1024px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)"
+          ></link>
+          <link
+            rel="apple-touch-startup-image"
+            href="images/splashscreens/ipadpro1_splash.png"
+            media="(min-device-width: 834px) and (max-device-width: 834px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)"
+          ></link>
+          <link
+            rel="apple-touch-startup-image"
+            href="images/splashscreens/ipadpro2_splash.png"
+            media="(min-device-width: 1024px) and (max-device-width: 1024px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)"
+          ></link>
 
           {/* <meta
             name="viewport"
@@ -31,10 +79,6 @@ class MyDocument extends Document {
             // crossorigin
             href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.5/dist/web/static/pretendard.css"
           />
-          {/* <script type="module">
-            import 'https://cdn.jsdelivr.net/npm/@pwabuilder/pwaupdate'; const el =
-            document.createElement('pwa-update'); document.body.appendChild(el);
-          </script> */}
         </Head>
         <body>
           <Main />
