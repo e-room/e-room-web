4247782d5e595a0c14d469b8d4dae8a5aa1302fc
injulme <injulme0309@naver.com>
2023-02-22 22:17:18 +0900

    mobile height 설정

--- pages/_app.js
+++ pages/_app.js
@@ -3,8 +3,17 @@ import "styles/reset.css";
 import "styles/palette.css";
 
 import { RecoilRoot } from "recoil";
+import { useEffect } from "react";
 
 const App = ({ Component, pageProps }) => {
+  function setScreenSize() {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  }
+  useEffect(() => {
+    setScreenSize();
+  });
+
   return (
     <RecoilRoot>
       <title>e-room</title>
--- styles/globalStyle.js
+++ styles/globalStyle.js
@@ -2,9 +2,12 @@ import { Global, css } from "@emotion/react";
 const globalStyle = css`
   /* Typography */
   * {
-    font-family: Pretendard, -apple-system, BlinkMacSystemFont, system-ui,
-      Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo",
-      "Noto Sans KR", "Malgun Gothic", sans-serif !important;
+    font-family: Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto,
+      "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic",
+      sans-serif !important;
+  }
+  :root {
+    --vh: 100%;
   }
 `;
 export const GlobalStyle = () => {
