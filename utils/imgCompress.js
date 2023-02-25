45f71c468db69d9fdf9246360d04a3866b43ead1
injulme <injulme0309@naver.com>
2023-02-25 23:44:27 +0900

    이미지 압축 사이즈 조절해봄

--- utils/imgCompress.js
+++ utils/imgCompress.js
@@ -2,8 +2,8 @@ import imageCompression from "browser-image-compression";
 
 export default async (file) => {
   const options = {
-    maxSizeMB: 1,
-    maxWidthOrHeight: 700,
+    maxSizeMB: 1.2,
+    maxWidthOrHeight: 900,
     useWebWorker: true,
   };
   return await imageCompression(file, options);
