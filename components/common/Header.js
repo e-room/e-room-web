979a5f0a1c203246f5d568b30a67cf76d4f37803
injulme <injulme0309@naver.com>
2023-05-30 21:10:55 +0900

    header tailwind 마이그레이션 완료

--- components/common/Header.js
+++ components/common/Header.js
@@ -1,5 +1,3 @@
-import styled from "@emotion/styled";
-import { SubTitle2 } from "../../styles/typography";
 import Icon from "../common/atoms/Icon";
 import { useRouter } from "next/router";
 
@@ -14,78 +12,36 @@ export default function Header({ pageTitle, additionalFunction }) {
     }
   };
 
+  const styleByBox = "flex justify-center items-center min-w-[24px]";
+
   return (
-    <AppBarWrapper>
+    <header className="w-full h-[44px] fixed top-0 left-0 pr-[20px] pl-[16px] box-border bg-white z-10 flex justify-between items-center shadow-header backdrop-blur-[12px]">
       {router.pathname === "/mypage" ||
       router.pathname === "/favorites" ||
       router.pathname === "/" ? (
-        <Box></Box>
+        <div className={styleByBox}></div>
       ) : (
-        <Box onClick={onBack} className="cursor-pointer">
+        <div className={styleByBox + "cursor-pointer"} onClick={onBack}>
           <Icon icon={"arrow-left"} size="md" />
-        </Box>
+        </div>
       )}
-      <Box>
+      <div className={styleByBox}>
         {pageTitle ? (
-          <HeaderTitle>{pageTitle}</HeaderTitle>
+          <div className="text-subtitle-2 text-black break-words truncate">
+            {pageTitle}
+          </div>
         ) : (
           <>
             <Icon icon={"logo-default"} size="lg" />
-            <LogoTitle>
-              <span>e</span>room
-            </LogoTitle>
+            <div className="text-subtitle-2 text-primary-1">
+              <span className="text-primary-3">e</span>room
+            </div>
           </>
         )}
-      </Box>
-      <Box className="cursor-pointer">{additionalFunction ?? null}</Box>
-    </AppBarWrapper>
+      </div>
+      <div className={styleByBox + "cursor-pointer"}>
+        {additionalFunction ?? null}
+      </div>
+    </header>
   );
 }
-
-const AppBarWrapper = styled.header`
-  width: 100%;
-  height: 44px;
-
-  position: fixed;
-  top: 0;
-  left: 0;
-  padding: 0 20px 0 16px;
-  box-sizing: border-box;
-
-  background: var(--white);
-  z-index: 9;
-  box-shadow: 0px -4px 16px 0px rgba(0, 0, 0, 0.04);
-
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-`;
-
-const Box = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  min-width: 24px;
-`;
-
-const HeaderTitle = styled.div`
-  ${SubTitle2}
-  text-align: center;
-
-  color: var(--black);
-
-  word-break: break-word;
-  text-overflow: ellipsis;
-  overflow: hidden;
-  display: -webkit-box;
-  -webkit-line-clamp: 1;
-  -webkit-box-orient: vertical;
-`;
-
-const LogoTitle = styled.div`
-  ${SubTitle2}
-  color: var(--primary-1);
-  span {
-    color: var(--primary-3);
-  }
-`;
