46005a85663b58487cba37f91e98ea29fc9d1825
injulme <injulme0309@naver.com>
2023-05-31 21:01:39 +0900

    select 컴포넌트 tailwind 적용

--- components/common/atoms/Select.js
+++ components/common/atoms/Select.js
@@ -1,122 +1,55 @@
-import ArrowDownIcon from "../../../assets/arrow-down.svg";
-import ArrowUpIcon from "../../../assets/arrow-up.svg";
-import { Body2 } from "../../../styles/typography";
+import ArrowDownIcon from "assets/arrow-down.svg";
+import ArrowUpIcon from "assets/arrow-up.svg";
 import Select, { components } from "react-select";
 
-// size = md || lg
 export default function CustomSelect({
-  size = "lg",
+  size = "lg", // 'md' || 'lg'
   options,
   value,
   label,
   onChange,
 }) {
   const formatValue = label ? value : { value };
-  // TODO: padding size 15 -> 16
-  const colorStyles = {
-    container: (base) => {
-      return { ...base, width: "100%" };
-    },
-    control: (base, state) => {
-      return {
-        ...base,
-        padding: size === "lg" ? 15 : "9px 15px",
-        borderRadius: 12,
-
-        outline: "none",
-        borderWidth: 1,
-        borderColor:
-          state.isFocused && state.menuIsOpen
-            ? `var(--primary-1)`
-            : `var(--gray-4)`,
-        boxShadow: "none",
-        "&:hover": `var(--primary-1)`,
-      };
-    },
-    placeholder: (base) => {
-      return {
-        ...base,
-        Body2,
-        color: `var(--gray-3)`,
-        margin: 0,
-      };
-    },
-    singleValue: (base) => {
-      return { ...base, fontWeight: 500, fontSize: "16px", lineHeight: "24px" };
-    },
-    input: (base) => {
-      return {
-        ...base,
-        margin: 0,
-        padding: 0,
-      };
-    },
-    valueContainer: (base) => {
-      return { ...base, padding: 0 };
-    },
-    menu: (base) => {
-      return {
-        ...base,
-        boxShadow: "0px 0px 12px rgba(0, 0, 0, 0.08)",
-        borderRadius: 12,
-        backgroundColor: `var(--white)`,
-      };
-    },
-    menuList: (base) => {
-      return { ...base, padding: 0 };
-    },
-    option: (base, state) => {
-      const isActive = (state.isFocused && !state.data) || state.isSelected;
-      return {
-        ...base,
-        Body2,
-        fontWeight: isActive ? `700` : `500`,
-        color: isActive ? `var(--primary-1)` : `var(--black)`,
-        padding: 16,
-        backgroundColor: isActive ? `var(--primary-6)` : `var(--white)`,
-        ":nth-of-type(1)": {
-          borderRadius: "12px 12px 0px 0px",
-        },
-        ":nth-last-of-type(1)": {
-          borderRadius: "0px 0px 12px 12px",
-        },
-        "&:hover": {
-          backgroundColor: `var(--primary-6)`,
-          ":nth-of-type(1)": {
-            borderRadius: "12px 12px 0px 0px",
-          },
-          ":nth-last-of-type(1)": {
-            borderRadius: "0px 0px 12px 12px",
-          },
-        },
-        ":active": {
-          backgroundColor: `var(--primary-6)`,
-          ":nth-of-type(1)": {
-            borderRadius: "12px 12px 0px 0px",
-          },
-          ":nth-last-of-type(1)": {
-            borderRadius: "0px 0px 12px 12px",
-          },
-        },
-      };
-    },
-  };
 
   const DropdownIndicator = (props) => {
     const { selectProps } = props;
     return (
       <components.DropdownIndicator {...props}>
         {props.isFocused && selectProps.menuIsOpen ? (
-          <ArrowUpIcon width={11} height={6} fill={`var(--primary-1)`} />
+          <ArrowUpIcon width={11} height={6} className="fill-primary-1" />
         ) : (
-          <ArrowDownIcon width={11} height={6} fill={`var(--gray-1)`} />
+          <ArrowDownIcon width={11} height={6} className="fill-gray-1" />
         )}
       </components.DropdownIndicator>
     );
   };
   return (
     <Select
-      styles={colorStyles}
+      classNames={{
+        container: () => "w-full",
+        control: ({ isFocused, menuIsOpen }) =>
+          `rounded-[12px] outline-none border ${
+            isFocused && menuIsOpen ? "border-primary-1" : "border-gray-4"
+          } shadow-none ${size === "lg" ? "p-[15px]" : "py-[9px] px-[15px]"}`,
+        placeholder: () => "text-body-2 text-gray-3",
+        // input: () => "m-0 p-0",
+        // valueContainer: () => "p-0",
+        singleValue: () => "text-body-2",
+        menu: () => "shadow-select rounded-[12px] bg-white",
+        // menuList: () => "p-0",
+        option: ({ isFocused, data, isSelected }) => {
+          const isActive = (isFocused && !data) || isSelected;
+          return `text-body-2 p-[16px] ${
+            isActive
+              ? "font-bold text-primary-1 bg-primary-6"
+              : "font-medium text-black bg-white"
+          }
+          first-of-type:rounded-t-[12px] last-of-type:rounded-b-[12px]
+          hover:bg-primary-6 active:bg-primary-6
+          `;
+        },
+      }}
+      unstyled={true}
       components={{
         DropdownIndicator,
         IndicatorSeparator: null,
