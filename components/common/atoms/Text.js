9e1e7893a0e5bc436567ec27dc50b5c4c2b5f93c
injulme <injulme0309@naver.com>
2023-05-31 21:02:23 +0900

    text 컴포넌트 tailwind 적용

--- components/common/atoms/Text.js
+++ components/common/atoms/Text.js
@@ -1,6 +1,4 @@
 import PropTypes from "prop-types";
-import styled from "@emotion/styled";
-import { Body2, Body2Bold, Body3 } from "styles/typography";
 
 export default function Text({
   type = "text",
@@ -14,10 +12,11 @@ export default function Text({
   ...props
 }) {
   return (
-    <div style={{ display: "flex", flexDirection: "column" }}>
-      {label && <TextLabel>{label}</TextLabel>}
-      <div style={{ display: "flex", alignItems: "center" }}>
-        <StyledInputText
+    <div className="flex flex-col">
+      {label && <div className="text-body-bold-2 mb-[4px]">{label}</div>}
+      <div className="flex items-center">
+        <input
+          className="text-body-2 flex items-center p-[15px] border border-gray-4 rounded-[12px] appearance-none placeholder:text-gray-3 focus:outline-none focus:border-primary-1"
           type={type}
           placeholder={placeholder}
           onChange={onChange}
@@ -25,7 +24,11 @@ export default function Text({
           style={{ width, height }}
           {...props}
         />
-        {unit && <TextUnit>{unit}</TextUnit>}
+        {unit && (
+          <div className="text-body-3 ml-[8px] w-[30px] min-w-[30px] whitespace-nowrap">
+            {unit}
+          </div>
+        )}
       </div>
     </div>
   );
@@ -40,45 +43,3 @@ Text.propTypes = {
   label: PropTypes.string,
   unit: PropTypes.string,
 };
-
-const StyledInputText = styled.input`
-  ${Body2}
-  display: flex;
-  align-items: center;
-
-  // TODO: padding size 15 -> 16
-  padding: 15px;
-
-  border: 1px solid var(--gray-4);
-  border-radius: 12px;
-
-  appearance: none;
-  -webkit-appearance: none;
-  -moz-appearance: none;
-
-  ::placeholder {
-    color: var(--gray-3);
-  }
-  &:focus {
-    outline: none;
-    border: 1px solid var(--primary-1);
-  }
-  ::-webkit-inner-spin-button {
-    -webkit-appearance: none;
-    margin: 0;
-  }
-`;
-
-const TextLabel = styled.div`
-  ${Body2Bold}
-
-  margin-bottom: 4px;
-`;
-
-const TextUnit = styled.div`
-  ${Body3}
-  white-space: nowrap;
-  margin-left: 8px;
-  width: 30px;
-  min-width: 30px;
-`;
