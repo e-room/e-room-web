2ed0c326175f96309da9ac0be9140a3fa7e2f209
injulme <injulme0309@naver.com>
2023-07-03 22:18:14 +0900

    popup 라이브러리 추가 컴포넌트 재구현

--- components/common/atoms/Popup.js
+++ components/common/atoms/Popup.js
@@ -1,13 +1,10 @@
 import PropTypes from "prop-types";
-import styled from "@emotion/styled";
-import { SubTitle1 } from "styles/typography";
 import Button from "./Button";
 import { useEffect, useState } from "react";
-import { fadeIn_Down } from "styles/keyframes";
 
 export default function Popup({
   title,
-  titleAlign = "center",
+  titleAlign = "center", // left, center, right
   visible = false,
   buttonType = "default",
   onCancelClick,
@@ -17,6 +14,12 @@ export default function Popup({
   cancelText = "cancel",
   submitText = "submit",
 }) {
+  const stylesByTextAlign = {
+    left: "text-left",
+    center: "text-center",
+    right: "text-right",
+  };
+
   const ButtonType = () => {
     switch (buttonType) {
       case "confirm":
@@ -31,7 +34,7 @@ export default function Popup({
         );
       case "default":
         return (
-          <div style={{ display: "flex", gap: 8 }}>
+          <div className="flex gap-[8px]">
             <Button
               label={cancelText}
               size="md"
@@ -50,7 +53,7 @@ export default function Popup({
         );
       case "warning":
         return (
-          <div style={{ display: "flex", gap: 8 }}>
+          <div className="flex gap-[8px]">
             <Button
               label={cancelText}
               size="md"
@@ -72,37 +75,37 @@ export default function Popup({
     }
   };
 
-  const [isOpen, setIsOpen] = useState(false);
   useEffect(() => {
-    let timeoutId;
     if (visible) {
       document.body.style.overflow = "hidden";
-      setIsOpen(true);
-    } else {
-      timeoutId = setTimeout(() => setIsOpen(false), 300);
     }
 
     return () => {
       document.body.style.overflow = "unset";
-
-      if (timeoutId !== undefined) {
-        clearTimeout(timeoutId);
-      }
     };
   }, [visible]);
 
-  if (!isOpen) return null;
-
   return (
-    <Overlay>
-      <Container visible={visible}>
-        <StyledPopup visible={visible}>
-          <Title align={titleAlign}>{title}</Title>
+    <div className="w-full h-full fixed top-0 left-0 overflow-hidden touch-none z-20 bg-overlay">
+      <div
+        className={`h-full flex justify-center items-center ${
+          visible ? "animate-popup-visible" : "animate-popup-hidden delay-300"
+        }`}
+      >
+        <div
+          className="flex flex-col pt-[32px] px-[16px] pb-[16px] gap-[24px] isolate max-w-[320px] box-border bg-white rounded-[24px]"
+          style={{ width: "calc(100vw - 40px)" }}
+        >
+          <div
+            className={`text-subtitle-1 text-black ${stylesByTextAlign[titleAlign]}`}
+          >
+            {title}
+          </div>
           {children}
-          <ButtonGroup>{ButtonType()}</ButtonGroup>
-        </StyledPopup>
-      </Container>
-    </Overlay>
+          <div className="w-full">{ButtonType()}</div>
+        </div>
+      </div>
+    </div>
   );
 }
 
@@ -117,54 +120,3 @@ Popup.propTypes = {
   cancelText: PropTypes.string,
   submitText: PropTypes.string,
 };
-
-const Overlay = styled.div`
-  width: 100%;
-  height: 100%;
-  position: fixed;
-  top: 0;
-  left: 0;
-  overflow: hidden;
-  touch-action: none;
-  z-index: 11;
-
-  /** var(--black) and opacity 20% **/
-  background: rgba(33, 33, 33, 0.2);
-`;
-
-const Container = styled.div`
-  height: 100%;
-
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  ${(p) => fadeIn_Down(p.visible)}
-`;
-
-const StyledPopup = styled.div`
-  display: flex;
-  flex-direction: column;
-  padding: 32px 16px 16px;
-  gap: 24px;
-  isolation: isolate;
-
-  width: calc(100vw - 40px);
-  max-width: 320px;
-  box-sizing: border-box;
-
-  background: var(--white);
-  border-radius: 24px;
-`;
-
-const Title = styled.div`
-  ${SubTitle1}
-
-  color: var(--black);
-
-  text-align: ${(p) => p.align};
-`;
-
-const ButtonGroup = styled.div`
-  width: 100%;
-`;
