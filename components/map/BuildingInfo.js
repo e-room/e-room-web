69b959f9897a23d54c87552e1f3653c59b1e0b35
injulme <injulme0309@naver.com>
2023-07-04 00:33:01 +0900

    qa14 완료

--- components/map/BuildingInfo.js
+++ components/map/BuildingInfo.js
@@ -1,8 +1,4 @@
-import styled from "@emotion/styled";
-
-import { Body2Bold, Caption2, Caption2Bold } from "styles/typography";
-import Img from "assets/img.png";
-
+import Logo from "assets/pic.png";
 import Score from "components/common/atoms/Score";
 import parseFloat from "utils/parseFloat";
 import axios from "axios";
@@ -42,14 +38,24 @@ export default ({ id }) => {
   }, [id]);
 
   return (
-    <Container onClick={goBuildingDetailView}>
-      <img src={Img.src} width={72} height={72} style={{ borderRadius: 8 }} />
-      <div className="wrapper">
-        <div className="building-name">{building.name}</div>
-        <ReviewArea>
-          <div className="review-count">리뷰 {building.reviewCnt}개</div>
-          <StarArea>{parseFloat(building.avgScore, 1)}</StarArea>
-          <div style={{ marginTop: 3 }}>
+    <div
+      className="flex gap-[12px] cursor-pointer bg-white max-w-[320px] max-h-[88px] box-border rounded-[12px] p-[8px] mt-[8px]"
+      style={{ width: "calc(100vw - 40px)" }}
+      onClick={goBuildingDetailView}
+    >
+      <img src={Logo.src} width={72} height={72} className="rounded-[8px]" />
+      <div className="flex flex-col justify-center gap-[4px]">
+        <div className="text-body-bold-2 text-black min-h-[24px]">
+          {building.name}
+        </div>
+        <div className="flex items-center max-h-[16px]">
+          <div className="text-caption-2 mr-[8px] text-black/50">
+            리뷰 {building.reviewCnt}개
+          </div>
+          <div className="text-caption-bold-2 text-primary-1">
+            {parseFloat(building.avgScore, 1)}
+          </div>
+          <div className="mt-[3px]">
             <Score
               size="sm"
               readOnly={true}
@@ -57,53 +63,8 @@ export default ({ id }) => {
               allowFraction={true}
             />
           </div>
-        </ReviewArea>
+        </div>
       </div>
-    </Container>
+    </div>
   );
 };
-
-const Container = styled.div`
-  width: calc(100vw - 40px);
-  background-color: white;
-  max-width: 320px;
-  max-height: 88px;
-  box-sizing: border-box;
-  border-radius: 12px;
-  padding: 8px;
-  margin-top: 8px;
-
-  cursor: pointer;
-
-  gap: 12px;
-  display: flex;
-
-  .wrapper {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    gap: 4px;
-  }
-
-  .building-name {
-    ${Body2Bold}
-    color: var(--black);
-    min-height: 24px;
-  }
-`;
-
-const ReviewArea = styled.div`
-  display: flex;
-  align-items: center;
-  max-height: 16px;
-  .review-count {
-    ${Caption2}
-
-    opacity: 0.5;
-    margin-right: 8px;
-  }
-`;
-const StarArea = styled.div`
-  ${Caption2Bold}
-  color: var(--primary-1);
-`;
