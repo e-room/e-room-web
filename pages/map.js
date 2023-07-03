9db38997ac3c373e4a11ad8c746ce732a25a2e89
injulme <injulme0309@naver.com>
2023-07-04 00:34:00 +0900

    naver map 마이그레이션

--- pages/map.js
+++ pages/map.js
@@ -1,205 +1,195 @@
-import React, { useEffect, useState, useRef, Fragment, useMemo } from "react";
+import { Fragment, useEffect, useRef, useState } from "react";
 import axios from "axios";
 import Link from "next/link";
-import styled from "@emotion/styled";
 
-import MarkerPng from "assets/marker4.png";
-
-import IconButton from "components/common/atoms/IconButton";
-import GroupButton from "components/common/atoms/GroupButton";
-import Button from "components/common/atoms/Button";
-import Footer from "components/common/Footer";
 import logEvent from "amplitude/logEvent";
-import { fadeInUp_OutDown } from "styles/keyframes";
+
+import Footer from "components/common/Footer";
+import Button from "components/common/atoms/Button";
+import GroupButton from "components/common/atoms/GroupButton";
+import IconButton from "components/common/atoms/IconButton";
 import BuildingInfo from "components/map/BuildingInfo";
-import SearchPage from "components/map/SearchPage";
 
-const MainMap = ({ data }) => {
+const map = ({ data }) => {
   const buildingMarking = JSON.parse(data);
-  const map = useRef(null);
+  const boundsListRef = useRef(null);
+  const mapElement = useRef(null);
+  const [map, setMap] = useState(null);
 
-  const [mapLoaded, setMapLoaded] = useState(false);
-  const imsiMarkerList = [];
-  const [searchVisible, setSearchVisible] = useState(false);
-  const [buttonVisible, setButtonVisible] = useState(true);
-  const [infoVisible, setInfoVisible] = useState({ visible: false, id: null });
+  const [test, setTest] = useState(null);
+  const testRef = useRef(null);
 
   useEffect(() => {
-    logEvent({ name: "view-map" });
-    const $script = document.createElement("script");
-    $script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_APP_KEY}&autoload=false&libraries=clusterer`;
-    $script.addEventListener("load", () => setMapLoaded(true));
-    document.head.appendChild($script);
+    const { naver } = window;
+    if (!mapElement.current || !naver) return;
+
+    const initialPosition = JSON.parse(
+      localStorage.getItem("e-room_initial_position")
+    );
+
+    const initialCenter = {
+      lat: initialPosition?.centerPoint?._lat ?? 37.2429616,
+      lng: initialPosition?.centerPoint?._lng ?? 127.0800525,
+    };
+    const initialZoom = initialPosition?.zoom ?? 12;
+
+    const mapOptions = {
+      center: new naver.maps.LatLng(initialCenter.lat, initialCenter.lng),
+      zoom: initialZoom,
+    };
+    const initialMap = new naver.maps.Map(mapElement.current, mapOptions);
+
+    setMap(initialMap);
   }, []);
 
   useEffect(() => {
-    if (!mapLoaded) return;
+    if (
+      !buildingMarking.buildingList ||
+      buildingMarking.buildingList.length < 1
+    )
+      return;
 
-    kakao.maps.load(async () => {
-      const centerPoint = JSON.parse(localStorage.getItem("centerPoint"));
-      const initial = {
-        lat: centerPoint?.lat ?? 37.2429616,
-        lng: centerPoint?.lng ?? 127.0800525,
-      };
+    setCenterPoint();
+    const markers = [];
+    const content = `<img src="/map/current-pin.png" width="61" height="65" alt="현재 위치"/>`;
+    const clusteringIcon = {
+      content:
+        '<div style="cursor:pointer;width:120px;height:120px;line-height:120px;font-size:16px;color:white;text-align:center;font-weight:700;background:url(/map/num-pin-one.png);background-size:contain;"></div>',
+      size: N.Size(120, 120),
+    };
 
-      var container = document.getElementById("map");
-      var options = {
-        center: new kakao.maps.LatLng(initial.lat, initial.lng),
-        level: centerPoint?.level ?? 8,
-      };
-
-      let imageSrc = MarkerPng.src;
-      let imageSize = new kakao.maps.Size(61, 68);
-      let imageOption = { offset: new kakao.maps.Point(30, 48) };
-      var markerImage = new kakao.maps.MarkerImage(
-        imageSrc,
-        imageSize,
-        imageOption
+    buildingMarking.buildingList.forEach((value) => {
+      const position = new naver.maps.LatLng(
+        value.coordinateDto.latitude,
+        value.coordinateDto.longitude
       );
 
-      map.current = await new kakao.maps.Map(container, options);
-
-      // 마커 클러스터러를 생성합니다
-      const clusterer = await new kakao.maps.MarkerClusterer({
-        map: map.current, // 마커들을 클러스터로 관리하고 표시할 지도 객체
-        averageCenter: true, // 클러스터에 포함된 마커들의 평균 위치를 클러스터 마커 위치로 설정
-        minLevel: 5, // 클러스터 할 최소 지도 레벨
-        styles: MarkerClustererStyles,
-        gridSize: 88,
+      let marker = new naver.maps.Marker({
+        position,
+        map,
+        icon: {
+          content,
+          size: new naver.maps.Size(61, 68),
+          origin: new naver.maps.Point(0, 0),
+        },
       });
 
-      const markers = [];
-
-      await buildingMarking.buildingList.forEach((value) => {
-        let marker = new kakao.maps.Marker({
-          position: new kakao.maps.LatLng(
-            value.coordinateDto.latitude,
-            value.coordinateDto.longitude
-          ),
-          clickable: true,
-          image: markerImage,
+      naver.maps.Event.addListener(marker, "click", () => {
+        logEvent({
+          name: "click-map-pin",
+          property: { buildingID: value.buildingId },
         });
-        marker.setMap(map.current);
-        kakao.maps.event.addListener(marker, "click", function () {
-          logEvent({
-            name: "click-map-pin",
-            property: { buildingID: value.buildingId },
-          });
+        map.panTo(position);
+        setTest(value.buildingId);
 
-          let clickPosition = new kakao.maps.LatLng(
-            value.coordinateDto.latitude,
-            value.coordinateDto.longitude
-          );
-          map.current.panTo(clickPosition);
-
-          setInfoVisible({ visible: true, id: value.buildingId });
-        });
-        markers.push(marker);
-        imsiMarkerList.push({ marker, id: value.buildingId });
+        testRef.current.classList.remove("hidden");
+        testRef.current.classList.add("animate-toast-visible");
       });
-      clusterer.addMarkers(markers);
 
-      setCenterPoint();
-      setLocalStorage();
+      markers.push({ marker, buildingId: value.buildingId });
+    });
 
-      kakao.maps.event.addListener(map.current, "idle", () => {
+    new MarkerClustering({
+      minClusterSize: 2,
+      maxZoom: 13,
+      map: map,
+      markers: markers.map((v) => v.marker),
+      disableClickZoom: false,
+      gridSize: 88,
+      icons: [clusteringIcon],
+      indexGenerator: [10, 100, 1000],
+      stylingFunction: function (clusterMarker, count) {
+        clusterMarker.getElement().querySelector("div:first-child").innerText =
+          count;
+      },
+    });
+
+    if (markers.length > 0) {
+      setLocalStorage(markers);
+    }
+
+    if (naver && map) {
+      naver.maps.Event.addListener(map, "idle", () => {
         setCenterPoint();
-        setLocalStorage();
-        onShowListButton();
+        setLocalStorage(markers);
       });
-    });
-  }, [mapLoaded, searchVisible]);
-
-  const onShowListButton = () => {
-    const bounds = map.current.getBounds();
-    const inBounds = imsiMarkerList.filter((overlay) => {
-      if (bounds.contain(overlay.marker.getPosition())) {
-        return overlay.id;
-      }
-    });
-    if (inBounds.length > 0) {
-      setButtonVisible(true);
-    } else {
-      setButtonVisible(false);
-      setInfoVisible({ visible: false, id: null });
     }
-  };
+  }, [map]);
 
-  const setLocalStorage = () => {
-    if (map.current) {
-      const bounds = map.current.getBounds();
-      const inBounds = imsiMarkerList.filter((overlay) => {
-        if (bounds.contain(overlay.marker.getPosition())) {
-          return overlay.id;
-        }
-      });
-      const ids = [];
-      inBounds.forEach((value) => ids.push(value.id));
-      localStorage.setItem("buildingMarking", ids);
-    }
-  };
   const setCenterPoint = () => {
-    if (map.current) {
-      const centerPoint = map.current.getCenter();
-      const level = map.current.getLevel();
+    if (map) {
+      const centerPoint = map.getCenter();
+      const zoomLevel = map.getZoom();
 
       localStorage.setItem(
-        "centerPoint",
+        "e-room_initial_position",
         JSON.stringify({
-          lat: centerPoint.getLat(),
-          lng: centerPoint.getLng(),
-          level: level,
+          centerPoint: centerPoint,
+          zoom: zoomLevel,
         })
       );
     }
   };
 
-  //나의 위치로 가게 해주는 함수
+  const setLocalStorage = (markers) => {
+    if (map) {
+      const mapBounds = map.getBounds();
+      let marker, position;
+      const ids = [];
+
+      for (let i = 0; i < markers.length; i++) {
+        marker = markers[i].marker;
+        position = marker.getPosition();
+
+        if (mapBounds.hasLatLng(position)) {
+          ids.push(markers[i].buildingId);
+        }
+      }
+
+      if (ids.length > 0) {
+        boundsListRef.current.classList.add("animate-toast-visible");
+        boundsListRef.current.classList.remove("animate-toast-hidden");
+      } else {
+        boundsListRef.current.classList.add("animate-toast-hidden");
+        boundsListRef.current.classList.remove("animate-toast-visible");
+        testRef.current.classList.add("animate-toast-hidden");
+        testRef.current.classList.add("hidden");
+        testRef.current.classList.remove("animate-toast-visible");
+      }
+    }
+  };
+
   const setMyPosition = () => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition((position) => {
         const lat = position.coords.latitude; // 위도
         const lng = position.coords.longitude; // 경도
 
-        let myPosition = new kakao.maps.LatLng(lat, lng);
-        map.current.setCenter(myPosition);
-        map.current.setLevel(1);
+        let myPosition = new naver.maps.LatLng(lat, lng);
+        map.panTo(myPosition);
+        map.setZoom(18, true);
       });
     }
   };
 
-  //줌인
   const zoomIn = () => {
-    map.current.setLevel(map.current.getLevel() - 1);
+    map.setZoom(map.getZoom() + 1, true);
   };
-  //줌아웃
   const zoomOut = () => {
-    map.current.setLevel(map.current.getLevel() + 1);
+    map.setZoom(map.getZoom() - 1, true);
   };
 
-  const buildingView = useMemo(() => {
-    return <BuildingInfo id={infoVisible.id} />;
-  }, [infoVisible]);
-
-  if (searchVisible) {
-    return (
-      <SearchPage
-        searchVisible={searchVisible}
-        setSearchVisible={setSearchVisible}
-      />
-    );
-  }
   return (
     <Fragment>
       <div className="relative overflow-hidden w-screen h-screen m-0">
-        <div className="w-screen h-screen" id="map" />
+        <div className="w-screen h-screen" ref={mapElement} />
         <div className="absolute right-[20px] top-[25px] z-[2]">
           <IconButton
             icon="search"
-            onClick={() => {
-              logEvent({ name: "click-map-search" });
-              setSearchVisible(true);
-            }}
+            // onClick={() => {
+            //   logEvent({ name: "click-map-search" });
+            //   setSearchVisible(true);
+            // }}
           />
         </div>
         <div className="absolute right-[20px] top-[232px] z-[2]">
@@ -213,7 +203,10 @@ const MainMap = ({ data }) => {
         <div className="absolute right-[20px] top-[328px] z-[2]">
           <IconButton onClick={setMyPosition} icon="location" />
         </div>
-        <ButtonItem visible={buttonVisible} infoVisible={infoVisible.visible}>
+        <div
+          className="fixed bottom-[64px] w-full flex justify-center items-center flex-col z-[2]"
+          ref={boundsListRef}
+        >
           <Link href={"/buildings"}>
             <a>
               <Button
@@ -224,10 +217,8 @@ const MainMap = ({ data }) => {
               />
             </a>
           </Link>
-          <Test visible={infoVisible.visible}>
-            {infoVisible.visible && buildingView}
-          </Test>
-        </ButtonItem>
+          <div ref={testRef}>{test && <BuildingInfo id={test} />}</div>
+        </div>
       </div>
       <Footer enabled={true} />
     </Fragment>
@@ -245,63 +236,4 @@ export async function getServerSideProps(context) {
   };
 }
 
-const MarkerClustererStyles = [
-  {
-    width: "120px",
-    height: "120px",
-    backgroundImage: "url(num-pin-one.png)",
-    backgroundSize: "120px 120px",
-    backgroundRepeat: "no-repeat",
-    borderRadius: "8px",
-    color: "white",
-    textAlign: "center",
-    fontWeight: "700",
-    lineHeight: "120px",
-    fontSize: "16px",
-  },
-  {
-    width: "120px",
-    height: "120px",
-    backgroundImage: "url(num-pin-two.png)",
-    backgroundSize: "120px 120px",
-    backgroundRepeat: "no-repeat",
-    borderRadius: "8px",
-    color: "white",
-    textAlign: "center",
-    fontWeight: "700",
-    lineHeight: "120px",
-    fontSize: "16px",
-  },
-  {
-    width: "120px",
-    height: "120px",
-    backgroundImage: "url(num-pin-three.png)",
-    backgroundSize: "120px 120px",
-    backgroundRepeat: "no-repeat",
-    borderRadius: "8px",
-    color: "white",
-    textAlign: "center",
-    fontWeight: "700",
-    lineHeight: "120px",
-    fontSize: "16px",
-  },
-];
-
-const ButtonItem = styled.div`
-  position: fixed;
-  bottom: 64px;
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  z-index: 2;
-
-  ${(p) => fadeInUp_OutDown(p.visible)}
-`;
-
-const Test = styled.div`
-  ${(p) => fadeInUp_OutDown(p.visible)}
-`;
-
-export default MainMap;
+export default map;
