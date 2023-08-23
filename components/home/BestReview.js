a8346338744528ddbecfb380d27d7f2bfac92e58
정효진 <injulme0309@naver.com>
2023-08-23 22:57:17 +0900

    feat: best review
    image slider 기능 작업중

--- components/home/BestReview.js
+++ components/home/BestReview.js
@@ -3,9 +3,12 @@ import Avatar from "components/common/atoms/Avatar";
 import Chip from "components/common/atoms/Chip";
 import Icon from "components/common/atoms/Icon";
 import parseFloat from "utils/parseFloat";
+import ImageList from "components/building/ImageList";
+import { useState } from "react";
+import Slider from "components/building/Slider";
 
 export default function BestReview({ data }) {
-  console.log("data", data);
+  console.log("data", data.reviewImageListDto.reviewImageList);
   const {
     score,
     advantage,
@@ -26,27 +29,50 @@ export default function BestReview({ data }) {
     },
   ];
 
+  const images = data.reviewImageListDto.reviewImageList;
+  const image = data.reviewImageListDto.reviewImageList[0];
+  const [showImages, setShowImages] = useState(false);
+
   return (
     <div className="flex flex-col gap-[16px] rounded-[12px] bg-white shadow-quicklink p-[16px] min-w-[calc(100vw-56px)] md:min-w-[667px]">
-      <div>
-        <div className="text-body-bold-2 text-black mb-[4px]">
-          {data.buildingName === ""
-            ? `${data.address.roadName} ${data.address.buildingNumber}`
-            : data.buildingName}
-        </div>
-        <div className="flex gap-[6px] items-center">
-          <Avatar size={"md"} img={data.authorDto.picture} />
-          <div className="text-caption-2 text-gray-1">
-            {data.authorDto.name}
+      <div className="justify-between flex gap-[16px]">
+        <div>
+          <div className="text-body-bold-2 text-black mb-[4px]">
+            {data.buildingName === ""
+              ? `${data.address.roadName} ${data.address.buildingNumber}`
+              : data.buildingName}
           </div>
-          <div className="flex gap-[4px]">
-            <Icon icon="star-filled" size="sm" fill="fill-primary-1" />
-            <div className="text-caption-bold-2 text-primary-1">
-              {parseFloat(score, 1)}
+          <div className="flex gap-[6px] items-center">
+            <Avatar size={"md"} img={data.authorDto.picture} />
+            <div className="text-caption-2 text-gray-1">
+              {data.authorDto.name}
+            </div>
+            <div className="flex gap-[4px]">
+              <Icon icon="star-filled" size="sm" fill="fill-primary-1" />
+              <div className="text-caption-bold-2 text-primary-1">
+                {parseFloat(score, 1)}
+              </div>
             </div>
           </div>
         </div>
+        <div>
+          <img
+            src={image.url}
+            width={52}
+            height={52}
+            className="rounded-[8px] cursor-pointer object-cover min-w-[52px] min-h-[52px] max-w-[52px] max-h-[52px]"
+            onClick={() => setShowImages(true)}
+          />
+        </div>
       </div>
+      {images.length > 0 && showImages && (
+        <Slider
+          data={images}
+          onClose={() => setShowImages(false)}
+          // defaultId={showImgDetail.uuid}
+        />
+      )}
+
       {advantages.map((v) => {
         return (
           <div className="mt-[16px]" key={v.title}>
